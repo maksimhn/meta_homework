b2a787519cb4b141e8169e7d28d535066dd1a8c7
Maksim <dollydasheep@gmail.com>
2015-06-26 09:43:40 -0400

    Adds test function and 'sameLineDistance' function.
    Calculates the distance between two stops at the same line.

--- mbta_classrom.js
+++ mbta_classrom.js
@@ -12,3 +12,21 @@ var find = function (array, value) {
   return -1;
 };
 
+var sameLineDistance = function (line, firstStop, secondStop) {
+  // find the indices of first and second stop and subtract them
+  var diff = find(line, firstStop) - find(line, secondStop);
+  if (diff <0) {
+    diff *= -1;
+  }
+  return diff;
+  // ternary operator:
+  // (diff > 0) ? diff : -diff;
+};
+
+var tester = function () {
+  console.log("find");
+  console.log("* should return 2, actually returns " + find(['a', 'b', 'c'], 'c'));
+  console.log("* should return -1, actually returns " + find(['a', 'b', 'c'], 'z'));
+  console.log("sameLineDistance");
+  console.log("should return 1, actually returns " + sameLineDistance(red, "south station", "park st"));
+};
